16015d85e3cf080e4f3638ebb36321bb6dcde289
mlapinm <mlapin@rambler.ru>
2022-03-11 15:00:08 +0300

    b36env python

--- b31042django/b04req.py
+++ b31042django/b04req.py
@@ -7,24 +7,35 @@ if __name__ == "__main__":
     "/routing/simple_route/blabla", # 404 
     "/routing/simple_route/", # 405 post
     "/routing/simple_route/", # 405 put
+    "/routing/slug_route/a-1s_d2/",
+    "/routing/slug_route/.4/24][/",
+    "/routing/sum_route/1/2/",
+    "/routing/sum_route/1/-2/",
+    "/routing/sum_route/1/b/",
+    "/routing/sum_route/a/2/",
+    "/routing/sum_get_method/",
+    "/routing/sum_get_method/?a=1&b=-2",
+    "/routing/sum_get_method/?a=1&b=b",
+    "/routing/sum_get_method/",
+    "/routing/sum_post_method/",
     "",
     "",
   ]
   url = "http://127.0.0.1:8000"
-  req = reqs[1]
-  res = requests.get(url + req)
-  print(res.text)
-  print(res.status_code)
-  req = reqs[3]
-  res = requests.post(url + req, data={'key':'value'})
-  print(res.text)
-  print(req, res.status_code)
-  res = requests.put(url + req, data={'key':'value'})
-  print(res.text)
-  print(req, res.status_code)
+  num = 15
+  for i in range(num, num + 1):
+    req = reqs[i]
+    if i == 3:
+      res = requests.post(url + req, data={'key':'value'})
+    elif i == 4:
+      res = requests.put(url + req, data={'key':'value'})
+    elif req == '/routing/sum_post_method/':
+      res = requests.post(url + req, data={'a': 1, 'b': '2'})
+    else:
+      res = requests.get(url + req)
 
+    print(res.text)
+    print(req, res.status_code)
 
-  # print(dir(requests))
-  # print(help())
 
 
