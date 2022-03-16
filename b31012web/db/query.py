6aa7f8d1b9b1757ddfe5a2be90b819a500536a5b
mlapinm <mlapin@rambler.ru>
2022-03-16 09:40:01 +0300

    isnull

--- b31012web/db/query.py
+++ b31012web/db/query.py
@@ -23,8 +23,14 @@ def create():
     entry1 = Entry(blog = blog1, headline = 'headline1')
     entry1.save()
     entry1.authors.add(a1)
+    entry2 = Entry(blog = blog1, headline = 'headline2')
+    entry2.save()
+    entry2.authors.add(a1)
+    # entry3 = Entry(blog = blog2, headline = 'headline2')
+    # entry3.save()
+    # entry3.authors.add(a1)
 
-    print('create')
+    print('create1')
 
 
 
@@ -47,52 +53,61 @@ def exclude():
     print(['{} {} {}'.format(e.blog.name, e.headline, e.authors.all()[0].name) for e in entries])
 
 def complex():
-
-    pass    
-
-
-
-
-def show_tables():
-
-    user_n = Author.objects.all().count()
-    user_names = [e.name for e in Author.objects.all()]
-    print(user_n, user_names)
-
-    blog_n = Blog.objects.all().count()
-    blog_titles = [e.name for e in Blog.objects.all()]
-    print(blog_n, blog_titles)
-
-    topic_n = Author.objects.all().count()
-    topics_titles = [e.blog.name for e in Entry.objects.all()]
-    print(topic_n, topics_titles)
+    entries = Entry.objects.filter(headline='headline1', pk__gt=20)
+    print(entries.query)
+    print(1, entries[0].pk, ['{} {} {}'.format(e.blog.name, e.headline, e.authors.all()[0].name) for e in entries])
 
 
-    print(4444)
-    pass
+def comlex_filter_q():
+    entries = Entry.objects.filter((Q(headline='headline1') | Q(headline='headline2')))
+    print(entries.query)
+    print(1, entries[0].pk, ['{} {} {}'.format(e.blog.name, e.headline, e.authors.all()[0].name) for e in entries])
+
+def contains():
+    '''
+    contains, icontains
+    startwith, istartwith
+    endwith, iendwith
+    in
+    gt, gte, lt, lte
+    range
+    isnull
+    '''
+    entries = Entry.objects.filter(headline__contains='head')
+    print(entries.query)
+    print(entries[0].pk, ['{} {} {}'.format(e.blog.name, e.headline, e.authors.all()[0].name) for e in entries])
 
 
+def relashinships():
+    entries = Entry.objects.filter(blog__name='blog1')
+    print(1, entries.query)
+    print(entries[0].pk, ['{} {} {}'.format(e.blog.name, e.headline, e.authors.all()[0].name) for e in entries])
 
+def reverse_relashionships():
+    blogs = Blog.objects.filter(entry__authors__name='a1')
+    print(2, blogs.query)
+    print(blogs[0].pk, ['{} '.format(e.name) for e in blogs])
 
-def delete_u1():
-    pass
+def distinct():
+    objects = Blog.objects.all()
+    entries = Entry.objects.all()
 
+    blogs1 = Blog.objects.filter(entry__headline__contains='headline')
+    print(['{} '.format(e.name) for e in blogs1])
 
-def unsubscribe_u2_from_blogs():
-    pass
+    blogs2 = Blog.objects.filter(entry__headline__contains='headline').distinct()
+    print(['{} '.format(e.name) for e in blogs2])
 
+def isnull():
+    objects = Blog.objects.all()
+    for e in objects:
+        entries = Entry.objects.filter(blog__name=e.name)
+        print(444, entries)
 
-def get_topic_created_grated():
-    pass
-
-
-def get_topic_title_ended():
-    pass
-
-
-def get_user_with_limit():
-    pass
-
+    entries = Entry.objects.all()
+    print(entries)
+    blogs = Blog.objects.filter(entry__headline__isnull=True)
+    blogs and print(blogs[0].name)
 
 def get_topic_count():
     pass
@@ -120,3 +135,18 @@ def get_topic_that_like_all_users():
 
 def get_topic_that_dont_have_like():
     pass
+
+def show_tables():
+
+    user_n = Author.objects.all().count()
+    user_names = [e.name for e in Author.objects.all()]
+    print(user_n, user_names)
+
+    blog_n = Blog.objects.all().count()
+    blog_titles = [e.name for e in Blog.objects.all()]
+    print(blog_n, blog_titles)
+
+    topic_n = Author.objects.all().count()
+    topics_titles = [e.blog.name for e in Entry.objects.all()]
+    print(topic_n, topics_titles)
+
