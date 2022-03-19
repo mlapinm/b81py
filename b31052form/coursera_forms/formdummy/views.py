355efcbefbd23d3c3af2283efa632c61edf6ea37
mlapinm <mlapin@rambler.ru>
2022-03-19 13:44:28 +0300

    fd2 form django

--- b31052form/coursera_forms/formdummy/views.py
+++ b31052form/coursera_forms/formdummy/views.py
@@ -2,25 +2,20 @@ from django.shortcuts import render
 from django.views import View
 
 from .schemaviews import FormSchemaView
+from .forms import DjangoForm
 
 class FormDjangoView(View):
-
     def get(self, request):
-        print(3333)
-        hello = request.GET.get('hello')
-        return render(request, 'form.html', {'hello': hello})
-
+        form = DjangoForm()
+        return render(request, 'formdj.html', {'form': form})
+    
     def post(self, request):
-        text = request.POST.get('text')
-        grade = request.POST.get('grade') 
-        image = request.FILES.get('image') 
-        content = image.read() 
-        context = {
-        'text': text, 
-        'grade': grade, 
-        'content': content
-        }
-        return render(request, 'form.html', context)
+        form = DjangoForm(request.POST) 
+        if form.is_valid():
+            context = form.cleaned_data
+            return render(request, 'formdj.html', context) 
+        else:
+            return render(request, 'error.html', {'error': form.errors})
 
 class FormSimpleView(View):
 
