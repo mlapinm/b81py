37b07937078629089acc333d63eff75e574e66b3
Mikhail Lapin <mlapin@rambler.ru>
2022-03-21 11:29:38 +0300

    fb6

--- b31052form/coursera_forms/feedback/urls.py
+++ b31052form/coursera_forms/feedback/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('add/', views.FeedbackCreateView.as_view(), name='feedback-create'),
+    path('feedback-list/', views.FcebackListView.as_view(), name='feedback-list'),
 ]
 
