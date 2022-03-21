df018b728d8c0d55d841b969a6d792675f6a3b03
mlapinm <mlapin@rambler.ru>
2022-03-21 08:56:27 +0300

    fb3 make migrations

--- b31052form/coursera_forms/feedback/models.py
+++ b31052form/coursera_forms/feedback/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+  ''' Отзыв о чем угодно. '''
+  text = models.CharField(verbose_name='Отзыв', max_length=5000)
+  grade = models.IntegerField(verbose_name='Оценка')
+  # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+  subject = models.CharField(verbose_name='Объект', max_length=100)
+
