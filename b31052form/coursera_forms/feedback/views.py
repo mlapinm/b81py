f9c1cae1ffcb0e452257c3910bebd3059da77fab
Mikhail Lapin <mlapin@rambler.ru>
2022-03-21 11:20:33 +0300

    fb5

--- b31052form/coursera_forms/feedback/views.py
+++ b31052form/coursera_forms/feedback/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.list import ListView
 from .models import Feedback
 
 # Create your views here.
+
+class FcebackListView(LoginRequiredMixin, ListView):
+  model = Feedback
+
+  def get_queryset(self):
+    if self.request.user.is_staff:
+      return Feedback.objects.all()
+    return Feedback.objects.filter(author=self.request.user)
 
 class FeedbackCreateView(LoginRequiredMixin, CreateView):
   model = Feedback
