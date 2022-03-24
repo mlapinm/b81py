0eef14d3b3b4ce673e4f730c761bfb2311e25878
Mikhail Lapin <mlapin@rambler.ru>
2022-03-24 16:07:48 +0300

    w5

--- b31042django/statsite/browser_sync.cmd
+++ b31042django/statsite/browser_sync.cmd
@@ -1,1 +1,8 @@
-browser-sync start --server
+@REM browser-sync start --server
+
+browser-sync start -s -f "*.html, *.css, *.php, css/*.css"
+
+@REM -s - browser-sync работает в режиме сервера
+@REM -f - отслеживает изменения в файлах
+@REM - *.html, *.php - изменения в файлах html и php
+@REM - изменения в файлах css в папке css
--- b31056auth/somemart/somemart/views.py
+++ b31056auth/somemart/somemart/views.py
@@ -7,40 +7,109 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator 
 from django.contrib.auth import authenticate, login
 
+from jsonschema import validate
+
+ITEM_SCHEMA = {
+  '$schema': 'http://json-schema.org/schema#', 
+  'type': 'object', 
+  'properties': {
+      "title": {
+          'type': 'string',
+          'minLength': 1,
+          'maxLength': 64, 
+      },
+      "description": {
+          'type': 'string',
+          'minLength': 1,
+          'maxLength': 1024, 
+      },
+      "price": {
+          'type': 'integer',
+          'minimum': 1, 
+          'maximum': 1000000 
+      },
+  },
+  'required': ['title', 'description', 'price']
+}
+
+REVIEW_SCHEMA = {
+  '$schema': 'http://json-schema.org/schema#', 
+  'type': 'object', 
+  'properties': {
+      "text": {
+          'type': 'string',
+          'minLength': 1,
+          'maxLength': 1024, 
+      },
+      "grade": {
+          'type': 'integer',
+          'minimum': 1, 
+          'maximum': 10 
+      },
+  },
+  'required': ['text', 'grade']
+}
 
 @method_decorator(csrf_exempt, name='dispatch') 
 class AddItemView(View):
     """View для создания товара."""
 
     def post(self, request):
-	# Здесь должен быть ваш код
-        print(55)
-        print(request.body)
-        print(555, request["username:password"])
-        print(request.user)
 
         print(request.META["HTTP_AUTHORIZATION"])
-        code = base64.b64encode(b'hello:world').decode()
-        print('code', code)
+        code = base64.b64encode(b'Aladdin:open sesame').decode()
+        print('code ', code)
 
-        str = 'Hello'
-        str_bytes = str.encode('utf-8')
-        print(str_bytes)
-        encoded = base64.b64encode(b'hello')
-        print(encoded)
+        try:
+            data = json.loads(request.body)
+            validate(data, ITEM_SCHEMA)
+        except:
+            return HttpResponse(status=400)
 
+        item = Item(**data)
+        item.save()
+        item_pk ='{}'.format(item.pk)
+        resp_data = {"id": item.pk}
+        return JsonResponse(resp_data, status=201)
 
-        # print(t)
-
-        data = {"aa": "bb"}
-        return JsonResponse(data, status=201)
-
-
+@method_decorator(csrf_exempt, name='dispatch') 
 class PostReviewView(View):
     """View для создания отзыва о товаре."""
 
+    @csrf_exempt    
     def post(self, request, item_id):
-	# Здесь должен быть ваш код
+        # rev = json.loads(request.body)
+        try:
+            data = json.loads(request.body)
+            validate(data, REVIEW_SCHEMA)
+        except:
+            return HttpResponse(status=400)
+
+        items = Item.objects.filter(pk=item_id)
+
+        review_item = {}
+        if items:
+            item = items[0]
+            # review_item = {
+            #     "grade": 3,
+            #     "text": "good",
+            #     "item": item,
+            # }
+            review_item = data
+            # print(review_item)
+            # review = Review(grade=review_item["grade"], text=review_item["text"])
+            review = Review()
+            review.text = review_item["text"]
+            review.grade = review_item["grade"]
+            review.item = item
+            review.save()
+            data = { "id": review.pk}
+
+            reviews = Review.objects.filter(item=item)
+            print(reviews.values())
+        else:
+            return HttpResponse(status=404)
+        # print(data)
         return JsonResponse(data, status=201)
 
 
@@ -52,5 +121,23 @@ class GetItemView(View):
     """
 
     def get(self, request, item_id):
-	# Здесь должен быть ваш код
+
+        data = {}
+        items = Item.objects.filter(pk=item_id)
+        if items:
+            item = items[0]
+            data = items.values()[0]
+            reviews = Review.objects.filter(item=item).order_by('-pk')[:5]
+            reviews.all()
+            reviews_values = [*reviews.values()]
+            # reviews_values.sort(key=lambda e: e['id'])
+            reviews_data = [{
+                "id":e["id"],
+                "grade":e["grade"],
+                "text":e["text"],
+                } for e in reviews_values]
+            data['reviews'] = reviews_data
+        else:
+            return HttpResponse("- 404 - товара с таким id: {} не существует.".format(item_id), status=404)
         return JsonResponse(data, status=200)
+
