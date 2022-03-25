62b7219550a730dca5c124a658f58daaaca18538
Mikhail Lapin <mlapin@rambler.ru>
2022-03-25 16:44:52 +0300

    bot

--- b31062bot/b08bot.py
+++ b31062bot/b08bot.py
@@ -7,6 +7,14 @@ token = '5162619904:AAFH6YahnWvGSzxzteRT0Tz9VJJaF8vEdMk'
 bot = telebot.TeleBot(token)
 items = []
 
+
+
+@bot.message_handler(commands=['start'])
+def handle_message(message):
+    print(message.text)
+    text = '/start, /add <message>, /list, /reset'
+    bot.send_message(message.chat.id, text)
+
 @bot.message_handler(commands=['add'])
 def handle_message(message):
     global items
@@ -21,6 +29,8 @@ def handle_message(message):
 @bot.message_handler(commands=['list'])
 def handle_message(message):
     global items
+    print(333)
+    print(bot.get_me().username)
     for e in items[-10:]:
         bot.send_message(message.chat.id, e)
         print(e)
@@ -35,4 +45,4 @@ def handle_message(message):
 
 bot.polling()
 
-s = '\add asdf'
+
