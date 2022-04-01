b8e7e62b4a7edebc854274206299f9810756f6e1
Mikhail Lapin <mlapin@rambler.ru>
2022-04-01 16:45:29 +0300

    form

--- b31052form/coursera_forms/feedback/models.py
+++ b31052form/coursera_forms/feedback/models.py
@@ -5,8 +5,8 @@ from django.db import models
 # Create your models here.
 class Feedback(models.Model):
   ''' Отзыв о чем угодно. '''
-  text = models.CharField(verbose_name='Отзыв', max_length=5000)
-  grade = models.IntegerField(verbose_name='Оценка')
+  text = models.CharField(verbose_name='Отзыв', max_length=5000, blank=True)
+  grade = models.IntegerField(verbose_name='Оценка', default=1)
   # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
   subject = models.CharField(verbose_name='Объект', max_length=100)
